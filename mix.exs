942b15f12fbf9ea5eebadec3f11a4185dea220de
Przemyslaw Roznawski <roznawski.przemyslaw@gmail.com>
2022-09-19 13:12:20 +0200

    release_v0.6.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Template.Mixfile do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_aac_plugin"
 
   def project do
