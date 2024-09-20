17dc5ec4e7d4dd81dedfc0d75cf08dc51c8009d7
noarkhh <jakub.pryc@gmail.com>
2024-09-20 09:28:01 +0200

    Fix putting lists into buffers

--- lib/membrane_element_rtp_aac/depayloader.ex
+++ lib/membrane_element_rtp_aac/depayloader.ex
@@ -34,8 +34,9 @@ defmodule Membrane.RTP.AAC.Depayloader do
 
   @impl true
   def handle_buffer(:input, buffer, _ctx, state) do
-    with {:ok, payload} <- Utils.parse_packet(buffer.payload, state.mode) do
-      {[buffer: {:output, %Buffer{buffer | payload: payload}}], state}
+    with {:ok, payloads} <- Utils.parse_packet(buffer.payload, state.mode) do
+      buffers = Enum.map(payloads, &%Buffer{buffer | payload: &1})
+      {[buffer: {:output, buffers}], state}
     else
       {:error, reason} -> raise "Cannot parse packet due to: #{inspect(reason)}"
     end
