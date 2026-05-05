7b6078f180882abca91a58b34d69e865dcb3beef
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-05 14:25:58 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.AAC.Mixfile do
   use Mix.Project
 
-  @version "0.9.5"
+  @version "0.9.6"
   @github_url "https://github.com/membraneframework/membrane_rtp_aac_plugin"
 
   def project do
