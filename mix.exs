dc0c7676c1225a47efccb3b882f43bbe6089d0ba
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2026-05-21 17:58:28 +0200

    Fix Apache license SPDX identifier format (#28)
    Change 'Apache 2.0' to 'Apache-2.0' to use the correct SPDX identifier.

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Membrane.RTP.AAC.Mixfile do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membrane.stream"
